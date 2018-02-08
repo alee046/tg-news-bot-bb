105e9722015766dd1c0838539aef7bf669fb2eba
Anthony Lee <alee9011@gmail.com>
2018-02-07 17:55:08 -0800

    changed timer to an hour

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ var interval = setInterval( () => {
         }, ( err ) => {
             console.log( err );
         }); 
-}, 10000 );
+}, 600000 );
 
 bot.onText( /\/spamdeezy/, ( msg, match ) => {
     const chatId = msg.chat.id;
